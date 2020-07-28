a7ecb35be237eb3b49508dc56c4df61e988c319b
jelmar <jelmarcambongga@gmail.com>
2020-07-28 08:27:32 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/jcchatapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jcchatapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JC CHAT APP',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Color(0xFFFEF9EB),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
